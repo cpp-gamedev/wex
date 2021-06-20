ff75774de21c694db255cf88283568c4a703e004
inJuly0 <srijannr1@gmail.com>
2021-06-20 13:39:10 +0530

    base setup

---
+++ include/wex/Game.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <SFML/System/Vector2.hpp>
+#include <string>
+#include <wex/common.hpp>
+
+namespace wex {
+
+class Driver {
+  public:
+	virtual void update(double dt) = 0;
+	virtual void draw()			   = 0;
+};
+
+class Game final {
+  public:
+	Game()			  = delete;
+	Game(Game&&)	  = delete;
+	Game(Game const&) = delete;
+	Game(Game&)		  = delete;
+	~Game()			  = default;
+
+	explicit Game(std::unique_ptr<Driver>&& driver) : mDriver(std::move(driver)){};
+
+	void run();
+
+  private:
+	void loop();
+
+  public:
+	static constexpr size_t DefaultWinWidth	  = 800;
+	static constexpr size_t DefaultWinHeight  = 600;
+	static constexpr double DefaultFPS		    = 60.0f;
+	static constexpr const char* DefaultTitle = "Wex Game";
+
+	const float mFps = DefaultFPS;
+	const std::string mWindowTitle{DefaultTitle};
+	const sf::Vector2u mWindowDim{DefaultWinWidth, DefaultWinHeight};
+
+  private:
+	std::unique_ptr<Driver> mDriver;
+};
+
+template <typename T, typename... Args> void dryRun(Args&&... args) {
+	static_assert(!std::is_abstract_v<T> and std::is_base_of_v<Driver, T>,
+				  "T must be a concrete class deriving from wex::Driver.");
+	auto driver_ptr = std::unique_ptr<T>(std::forward<Args>(args)...);
+	Game game(std::move(driver_ptr));
+	game.run();
+}
+
+} // namespace wex
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
+#include <wex/Game.hpp>
+
+class MyApp final : public wex::Driver {
+	void update([[maybe_unused]] double dt){};
+	void draw([[maybe_unused]] sf::RenderWindow window){};
+};
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-	window.setVerticalSyncEnabled(true);
-	window.setFramerateLimit(60);
-
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			switch (event.type) {
-			case sf::Event::Closed: window.close(); break;
-			case sf::Event::KeyPressed: std::cout << "ping\n"; break;
-			default: break;
-			}
-		}
-		window.clear();
-		window.display();
-	}
-	return EXIT_SUCCESS;
+	wex::dryRun<MyApp>();
+	return 0;
 }
